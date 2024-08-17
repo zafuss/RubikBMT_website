fa0477dd09414424fabdf832cf82b4286915c23f
vangggggggg <95358830+vangggggggg@users.noreply.github.com>
2024-08-17 19:40:40 +0700

    style: fix ui index backToSchool feat: validate Candidate

--- src/main/java/zafus/rubikbmt/rubikbmt_website/controllers/CandidateController.java
+++ src/main/java/zafus/rubikbmt/rubikbmt_website/controllers/CandidateController.java
@@ -1,13 +1,19 @@
 package zafus.rubikbmt.rubikbmt_website.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import zafus.rubikbmt.rubikbmt_website.entities.Candidate;
+import zafus.rubikbmt.rubikbmt_website.entities.Competition;
 import zafus.rubikbmt.rubikbmt_website.services.CandidateService;
+import zafus.rubikbmt.rubikbmt_website.services.CompetitionService;
 
 import java.time.LocalDateTime;
 
@@ -16,8 +22,26 @@ import java.time.LocalDateTime;
 public class CandidateController {
     @Autowired
     private CandidateService candidateService;
+    @Autowired
+    private CompetitionService competitionService;
+
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        return "/backToSchool/index";
+    }
+
     @PostMapping("/register")
-    public String register(@ModelAttribute Candidate candidate) {
+    public String register(@Valid @ModelAttribute Candidate candidate, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            var errors = bindingResult.getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).toArray(String[]::new);
+            Competition competition = competitionService.getByName("BackToSchool");
+            candidate.setCompetition(competition);
+            model.addAttribute("errors", errors);
+            model.addAttribute("events", competition.getEvents());
+            model.addAttribute("candidate",candidate);
+            return "/backToSchool/index";
+        }
         candidate.setConfirmed(false);
         candidate.setRegistrationTime(LocalDateTime.now());
         candidateService.add(candidate);
--- src/main/java/zafus/rubikbmt/rubikbmt_website/entities/Candidate.java
+++ src/main/java/zafus/rubikbmt/rubikbmt_website/entities/Candidate.java
@@ -2,10 +2,16 @@ package zafus.rubikbmt.rubikbmt_website.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -21,11 +27,19 @@ public class Candidate {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+    @Size(min = 1, max = 50, message = "Họ và tên đệm phải có từ 1 đến 50 ký tự")
     private String lastName;
+    @Size(min = 1, max = 50, message = " Tên phải có từ 1 đến 50 ký tự")
     private String firstName;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotNull(message = "Ngày tháng không được bỏ trống.")
     private LocalDate dateOfBirth;
+    @Length(min = 10, max = 10, message = "Phone phải có 10 số")
+    @Pattern(regexp = "^[0-9]*$", message = "Phone phải là số")
     private String phoneNumber;
-    private String gmail;
+    @Size(min = 1, max = 50, message = "Email phải có từ 1 đến 50 ký tự")
+    @Email(message = "Không đúng định dạng Email")
+    private String email;
 
     @ManyToOne
     private Competition competition;
